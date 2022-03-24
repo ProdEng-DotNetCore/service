df5091817f28e2997e40dad195b68affb34b560c
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2022-03-24 08:25:49 +0200

    3 integration tests i guess (#12)

---
+++ src/test/java/ro/unibuc/hello/controller/ProductControllerTestIT.java
@@ -0,0 +1,69 @@
+package ro.unibuc.hello.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ro.unibuc.hello.data.ProductEntity;
+import ro.unibuc.hello.data.ProductRepository;
+import ro.unibuc.hello.dto.AddProductDto;
+import ro.unibuc.hello.dto.ProductSellStockDto;
+import ro.unibuc.hello.exception.NotFoundException;
+
+@SpringBootTest
+public class ProductControllerTestIT {
+
+    @Autowired
+    ProductController productController;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @BeforeEach
+    public void setUp() {
+        var x = productRepository.findByTitle("Nu Este");
+        if(x != null)
+            productRepository.delete(x);
+
+        var y = productRepository.findByTitle("Este");
+        if(y != null)
+            productRepository.delete(y);
+
+        productRepository.save(new ProductEntity("Este", "Sigur ca este", 50));
+    }
+
+    @Test
+    @Order(1)
+    public void getProduct_Throws() {
+        try {
+            productController.getProduct("Nu Este");
+            Assertions.fail();
+        }
+        catch (Exception e){
+            Assertions.assertEquals(NotFoundException.class, e.getClass());
+            Assertions.assertEquals("Not Found", e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(2)
+    void addProduct() {
+        var product = new AddProductDto("Nu Este", "desc", 10);
+
+        productController.addProduct(product);
+
+        Assertions.assertNotNull(productRepository.findByTitle("Nu Este"));
+    }
+
+    @Test
+    @Order(3)
+    void sellProductStock() {
+
+        productController.sellProductStock(new ProductSellStockDto("Este", 49));
+
+        Assertions.assertEquals(1, productRepository.findByTitle("Este").quantity);
+    }
+
+}
