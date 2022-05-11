a49414a26c281decce6e3995f2f91b95d0ed8150
Andrei Tudose <andreitudose2000@gmail.com>
2022-05-11 22:29:26 +0300

    Added metrics and alerts relevant for app (#17)

--- src/main/java/ro/unibuc/hello/HelloApplication.java
+++ src/main/java/ro/unibuc/hello/HelloApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ro.unibuc.hello.data.ProductEntity;
 import ro.unibuc.hello.data.ProductRepository;
 
@@ -11,6 +12,7 @@ import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = ProductRepository.class)
+@EnableScheduling
 public class HelloApplication {
 
 	@Autowired
